ab0b0f3f564452dfd79c563b5b15cfa89d9ef88d
Alex Dodd <alexdodd@gmail.com>
2021-02-10 00:52:12 +0000

    Library constructor written bookCount and getCapacity passing tests

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> collection;
+    private int capacity;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        this.collection = new ArrayList<Book>();
+    }
+
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int getBookCount() {
+        return this.collection.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Library library;
+
+    @Before
+    public void before(){
+        book1 = new Book("Clean code", "Robert Martin", "Computing & Internet");
+        book2 = new Book("Effective Java", "Joshua Bloch", "Computing & Internet");
+        book3 = new Book("Idiomatic Python", "Jeff Knupp", "Computing & Internet");
+        library = new Library(10);
+    }
+
+    @Test
+    public void hasCapactiy(){
+        assertEquals(10, library.getCapacity());
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.getBookCount());
+    }
+
 }
